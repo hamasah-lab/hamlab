07c0a4a37b90101593d8da0be5ff7dcf0036e2c1
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2022-12-23 20:34:40 +0700

    Tailwind prettier plugin (#29)
    * chore: initial tailwind prettier plugin
    
    * chore: husky cant run
    
    * Setup Tailwindcss and Flowbite (#27)
    
    * chore: initial tailwind prettier plugin
    
    * fix: pnpm yaml
    
    * fix: commit-msg husky
    
    * chore: test tailwind prettier plugin
    
    * chore: ignore eslintcache
    
    * chore: fix pnpm lock
    
    Co-authored-by: Firman Justisio Lestari <jusles363@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import { Card } from 'flowbite-react'
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen min-w-full">
+    <div className="flex h-screen min-w-full items-center justify-center">
       <Card className="w-80">
-        <h5 className="text-2xl font-bold tracking-tight text-center text-gray-900 dark:text-white">Set Up Tailwind</h5>
+        <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Set Up Tailwind</h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, fuga cumque modi iusto nemo neque
         </p>
