302789534c0c8bac5b0dd53419e58631af1545c2
Firman Justisio Lestari <jusles363@gmail.com>
2022-12-23 21:41:41 +0700

    refactor: remove flowbite (#34)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-import { Card } from 'flowbite-react'
-
 const Home = () => {
   return (
     <div className="flex h-screen min-w-full items-center justify-center">
-      <Card className="w-80">
-        <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Set Up Tailwind</h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, fuga cumque modi iusto nemo neque
-        </p>
-      </Card>
+      <h1 className="text-xl font-bold">Hello World</h1>
     </div>
   )
 }
