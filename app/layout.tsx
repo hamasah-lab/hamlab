f1af905eeb612d7e92f50b2ef8a8fe078a277acf
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-12-25 08:26:18 +0700

    feat: using Nunito font from @next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import '~/styles/globals.css'
 
+import { Nunito } from '@next/font/google'
 import React from 'react'
+
+const nunito = Nunito({
+  variable: '--font-nunito',
+  subsets: ['latin'],
+})
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="id">
+    <html lang="id" className={nunito.variable}>
       <body>{children}</body>
     </html>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './public/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-nunito)'],
+      },
+    },
   },
   plugins: [],
 }
