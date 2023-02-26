864072cdfb9c07df515e0499f6a5c41fed047b7c
Firman Justisio Lestari <jusles363@gmail.com>
2023-02-26 19:45:50 +0700

    feat: initial breadcrumb

---
+++ src/components/layout/main/breadcrumb-header.tsx
@@ -0,0 +1,10 @@
+interface BreadcrumbHeaderProps {
+  breadcrumb: {
+    text: string
+    href?: string
+  }[]
+}
+
+const BreadcrumbHeader = ({ breadcrumb }: BreadcrumbHeaderProps) => <></>
+
+export default BreadcrumbHeader
--- src/components/layout/main/section-header.tsx
+++ src/components/layout/main/section-header.tsx
@@ -1,9 +1,18 @@
-// TODO: add breadcrumbs
+import BreadcrumbHeader from './breadcrumb-header'
 
-const SectionHeader = ({ title }: { title: string }) => {
+interface SectionHeaderProps {
+  title: string
+  breadcrumb?: {
+    text: string
+    href?: string
+  }[]
+}
+
+const SectionHeader = ({ title, breadcrumb }: SectionHeaderProps) => {
   return (
-    <div className="relative -mx-[30px] -mt-[10px] mb-[30px] flex items-center border-t border-t-gray-100 bg-white p-5 px-9 shadow-sm">
-      <h1 className="mb-0 mt-1 text-2xl font-bold text-gray-900">{title}</h1>
+    <div className="relative -mx-[30px] -mt-[10px] mb-[30px] flex items-center justify-between border-t border-t-gray-100 bg-white p-5 px-9 shadow-sm">
+      <h1 className="mt-1 mb-0 text-2xl font-bold text-gray-900">{title}</h1>
+      {breadcrumb && <BreadcrumbHeader breadcrumb={breadcrumb} />}
     </div>
   )
 }
