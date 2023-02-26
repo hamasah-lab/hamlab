9a3fdd5f6c24476b63b06c3aa73fe5b476d53a66
Firman Justisio Lestari <jusles363@gmail.com>
2023-02-26 19:52:41 +0700

    Forgot password page (#57)

---
+++ app/(auth)/forgot-password/head.tsx
@@ -0,0 +1,10 @@
+const Head = () => {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Forgot Password</title>
+    </>
+  )
+}
+
+export default Head
