c4087dec66f921d136c8909e9f29c0e2a2efa4d0
Firman Justisio Lestari <jusles363@gmail.com>
2023-02-26 11:07:36 +0700

    refactor: add title on head

--- app/(auth)/forgot-password/head.tsx
+++ app/(auth)/forgot-password/head.tsx
@@ -1,5 +1,10 @@
 const Head = () => {
-  return <></>
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Forgot Password</title>
+    </>
+  )
 }
 
 export default Head
