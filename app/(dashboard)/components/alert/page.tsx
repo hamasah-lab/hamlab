a1a2d5cd02302d9a4d5e7126ae82cadd22bcbdfd
Firman Justisio Lestari <jusles363@gmail.com>
2023-03-19 20:07:24 +0700

    feat: alert page

--- app/(dashboard)/components/alert/page.tsx
+++ app/(dashboard)/components/alert/page.tsx
@@ -1,5 +1,42 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { SectionTitle } from '~/dist/section-title'
+import useBreadcrumbContext from '~/hooks/use-breadcrumb-context'
+
 const AlertComponentPage = () => {
-  return <></>
+  const { setBreadcrumb } = useBreadcrumbContext()
+
+  useEffect(() => {
+    setBreadcrumb([
+      {
+        href: '/',
+        text: 'Dashboard',
+      },
+      {
+        href: '/',
+        text: 'General Components',
+      },
+      {
+        text: 'Alert',
+      },
+    ])
+  }, [setBreadcrumb])
+
+  return (
+    <div className="flex flex-col">
+      <SectionTitle
+        title="Alert"
+        subTitle="Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages."
+      />
+
+      <div className="flex gap-4">
+        <div className="relative flex max-w-[50%] shrink-0 grow-0 basis-1/2 flex-col gap-8"></div>
+        <div className="relative flex max-w-[50%] shrink-0 grow-0 basis-1/2 flex-col gap-8"></div>
+      </div>
+    </div>
+  )
 }
 
 export default AlertComponentPage
