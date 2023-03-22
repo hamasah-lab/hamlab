0129594bb7f2cfe0cd5836d782a53cd947077917
Firman Justisio Lestari <jusles363@gmail.com>
2023-03-22 21:22:36 +0700

    feat: alert component

--- app/(dashboard)/components/alert/page.tsx
+++ app/(dashboard)/components/alert/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useEffect } from 'react'
+import { MdOutlineLightbulb } from 'react-icons/md'
 
+import { Alert } from '~/dist/alert'
 import { Card, CardBody, CardHeader } from '~/dist/card'
 import { SectionTitle } from '~/dist/section-title'
 import useBreadcrumbContext from '~/hooks/use-breadcrumb-context'
@@ -37,11 +39,86 @@ const AlertComponentPage = () => {
           <Card>
             <CardHeader title="Default" />
             <CardBody>
-              <div className="flex flex-col"></div>
+              <div className="flex flex-col gap-y-4">
+                <Alert message="This is a primary alert." />
+                <Alert message="This is a secondary alert." variant="secondary" />
+                <Alert message="This is a success alert." variant="success" />
+                <Alert message="This is a danger alert." variant="danger" />
+                <Alert message="This is a warning alert." variant="warning" />
+                <Alert message="This is a info alert." variant="info" />
+                <Alert message="This is a light alert." variant="light" />
+                <Alert message="This is a dark alert." variant="dark" />
+              </div>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardHeader title="Icon" />
+            <CardBody>
+              <div className="flex flex-col gap-y-4">
+                <Alert message="This is a primary alert." Icon={MdOutlineLightbulb} />
+                <Alert message="This is a secondary alert." variant="secondary" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a success alert." variant="success" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a danger alert." variant="danger" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a warning alert." variant="warning" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a info alert." variant="info" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a light alert." variant="light" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a dark alert." variant="dark" Icon={MdOutlineLightbulb} />
+              </div>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardHeader title="Dismissable" />
+            <CardBody>
+              <div className="flex flex-col gap-y-4">
+                <Alert message="This is a primary alert." dissmissable={true} />
+                <Alert message="This is a secondary alert." variant="secondary" dissmissable={true} />
+                <Alert message="This is a success alert." variant="success" dissmissable={true} />
+                <Alert message="This is a danger alert." variant="danger" dissmissable={true} />
+                <Alert message="This is a warning alert." variant="warning" dissmissable={true} />
+                <Alert message="This is a info alert." variant="info" dissmissable={true} />
+                <Alert message="This is a light alert." variant="light" dissmissable={true} />
+                <Alert message="This is a dark alert." variant="dark" dissmissable={true} />
+              </div>
             </CardBody>
           </Card>
         </div>
-        <div className="relative flex max-w-[50%] shrink-0 grow-0 basis-1/2 flex-col gap-8"></div>
+        <div className="relative flex max-w-[50%] shrink-0 grow-0 basis-1/2 flex-col gap-8">
+          <Card>
+            <CardHeader title="Title" />
+            <CardBody>
+              <div className="flex flex-col gap-y-4">
+                <Alert message="This is a primary alert." title="Primary" />
+                <Alert message="This is a secondary alert." variant="secondary" title="Secondary" />
+                <Alert message="This is a success alert." variant="success" title="Success" />
+                <Alert message="This is a danger alert." variant="danger" title="Danger" />
+                <Alert message="This is a warning alert." variant="warning" title="Warning" />
+                <Alert message="This is a info alert." variant="info" title="Info" />
+                <Alert message="This is a light alert." variant="light" title="Light" />
+                <Alert message="This is a dark alert." variant="dark" title="Dark" />
+              </div>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardHeader title="Title with Icon" />
+            <CardBody>
+              <div className="flex flex-col gap-y-4">
+                <Alert message="This is a primary alert." title="Primary" Icon={MdOutlineLightbulb} />
+                <Alert
+                  message="This is a secondary alert."
+                  variant="secondary"
+                  title="Secondary"
+                  Icon={MdOutlineLightbulb}
+                />
+                <Alert message="This is a success alert." variant="success" title="Success" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a danger alert." variant="danger" title="Danger" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a warning alert." variant="warning" title="Warning" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a info alert." variant="info" title="Info" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a light alert." variant="light" title="Light" Icon={MdOutlineLightbulb} />
+                <Alert message="This is a dark alert." variant="dark" title="Dark" Icon={MdOutlineLightbulb} />
+              </div>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     </div>
   )
--- src/dist/alert/alert.tsx
+++ src/dist/alert/alert.tsx
@@ -1,7 +1,11 @@
+import { Transition } from '@headlessui/react'
 import { useState } from 'react'
+import { IconType } from 'react-icons'
 import { IoMdClose } from 'react-icons/io'
 
 import clsxm from '~/utils/clsxm'
+
+import { Text } from '../text'
 
 export type VariantAlert = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
 export interface PropsAlert {
@@ -10,6 +14,7 @@ export interface PropsAlert {
   className?: string
   dissmissable?: boolean
   variant?: VariantAlert
+  Icon?: IconType | 'success' | 'danger' | 'warning' | 'info'
 }
 export const ColorsBadge: Record<VariantAlert, { color: string; bgColor: string }> = {
   primary: {
@@ -47,22 +52,42 @@ export const ColorsBadge: Record<VariantAlert, { color: string; bgColor: string 
 }
 
 const Alert = (props: PropsAlert) => {
-  const [show, setShow] = useState(true)
-  const { message, className, dissmissable = false, title, variant = 'primary' } = props
+  const [isShowing, setIsShowing] = useState(true)
+  const { message, className, dissmissable = false, title, variant = 'primary', Icon } = props
   const { color, bgColor } = ColorsBadge[variant]
 
   return (
-    <>
-      {show && (
-        <div className={clsxm('flex flex-col rounded-md p-4', bgColor, className ?? '')}>
-          {title && <h1 className={clsxm('mb-3', color)}>{title}</h1>}
-          <div className="flex">
-            <p className={clsxm('flex flex-wrap', color)}>{message}</p>
-            {dissmissable && <div onClick={() => setShow(false)}>{<IoMdClose />}</div>}
+    <Transition show={isShowing} leave="transition-opacity duration-300" leaveFrom="opacity-100" leaveTo="opacity-0">
+      <div className={clsxm('flex rounded-md px-5 py-4', title ? '' : 'items-center', bgColor, className ?? '')}>
+        {Icon && typeof Icon != 'string' && (
+          <Icon className={clsxm(title ? 'mt-0.5 mr-4 h-7 w-7' : 'mb-0.5 mr-3 h-5 w-5', color)} />
+        )}
+        <div className="flex w-full flex-col">
+          {title && (
+            <Text fontSize="lg" className={clsxm('mb-2 font-bold', color)}>
+              {title}
+            </Text>
+          )}
+          <div className="flex justify-between">
+            <Text fontSize="sm" className={clsxm('flex flex-wrap font-normal', color)}>
+              {message}
+            </Text>
+            {dissmissable && (
+              <div onClick={() => setIsShowing(false)}>
+                {
+                  <IoMdClose
+                    className={clsxm(
+                      'h-5 w-5 cursor-pointer opacity-90 outline-none transition-opacity duration-200 hover:opacity-60 active:scale-125 active:duration-75',
+                      color
+                    )}
+                  />
+                }
+              </div>
+            )}
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </Transition>
   )
 }
 
