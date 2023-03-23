6d7050f6341f7af9e2d5ba514836f9d1e8612646
Firman Justisio Lestari <jusles363@gmail.com>
2023-03-23 11:34:32 +0700

    feat: favicon at auth page

--- app/(auth)/forgot-password/layout.tsx
+++ app/(auth)/forgot-password/layout.tsx
@@ -1,7 +1,15 @@
 import { PropsWithChildren } from 'react'
+import appleTouchIcon from 'src/assets/favicon/apple-touch-icon.png'
+import favicon16Icon from 'src/assets/favicon/favicon-16x16.png'
+import favicon32Icon from 'src/assets/favicon/favicon-32x32.png'
 
 export const metadata = {
   title: 'Forgot Password',
+  icons: [
+    { rel: 'apple-touch-icon', url: appleTouchIcon.src, type: 'images/png', sizes: '180x180' },
+    { rel: 'icon', url: favicon32Icon.src, type: 'images/png', sizes: '32x32' },
+    { rel: 'icon', url: favicon16Icon.src, type: 'images/png', sizes: '16x16' },
+  ],
 }
 
 const ForgotPasswordLayout = (props: PropsWithChildren) => {
